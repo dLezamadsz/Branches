5271d3edd19f0342bf18de22611ba0d20183d5df
D. Lezama <diavalez@gmail.com>
2026-04-16 17:59:53 -0500

    se crearon las frutas y los escenarios

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ namespace Aventura
     {
         static void Main(string[] args)
         {
-            
+            Manzana manz = new Manzana("Manzana", 3);
+            Durian dur = new Durian("Durian", -5);
+            Jugador player = new Jugador(4);
+            /* estás lejos de casa y tienes hambre
+             * manzana: +3, ya no tienes hambre, llegas a casa
+             * durian: -5, estaba pasada, game over
+             * ninguna: te quedas con hambre
+            */
+
             Console.WriteLine("Fuiste a caminar, pero te perdiste y estás lejos de casa. Tienes mucha hambre");
             Console.WriteLine("En el camino encuentras una canasta con dos frutas: una manzana y un durian. Estás contemplando tomar alguna para saciar tu hambre en el camino.");
             Console.WriteLine("1 - Manzana\n2 - Durian\n3 - Pasar de largo");
